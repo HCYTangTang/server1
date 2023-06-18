6a5423b6825186746b8d8ae0fa977ad2da69fa15
HCYTangTang <134395256+HCYTangTang@users.noreply.github.com>
2023-06-18 17:04:19 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const clientId = "SliOUwwJyx40KDhySwCf";
 const clientSecret = "r1Z3AkEecz";
 const apiUrl = "https://openapi.naver.com/v1/search/shop.json";
 
-app.get('/search/:keyword/:storeName/:start', async (req, res) => {
-  const { keyword, storeName, start } = req.params;
+app.get('/search/:keyword/:start', async (req, res) => {
+  const { keyword, start } = req.params;
   try {
     const response = await axios.get(apiUrl, {
       headers: {
@@ -25,15 +25,8 @@ app.get('/search/:keyword/:storeName/:start', async (req, res) => {
         display: 100,
       },
     });
-    
-    const items = response.data.items.map((item, index) => {
-      if (item.mallName === storeName) {
-        item.rank = start - 1 + index;
-      }
-      return item;
-    });
-    
-    res.json(items);
+
+    res.json(response.data.items);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error' });
@@ -41,21 +34,3 @@ app.get('/search/:keyword/:storeName/:start', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server Open'));
-
-function extractPopularDegree(html) {
-    const cheerio = require('cheerio');
-    const $ = cheerio.load(html);
-  
-    // 해당 상품 페이지에서 __NEXT_DATA__ 스크립트 태그를 찾아 JSON 파싱
-    const scriptTag = $('#__NEXT_DATA__');
-    const jsonData = JSON.parse(scriptTag.html());
-  
-    // popularDegree 값 반환
-    if (jsonData && jsonData.props && jsonData.props.pageProps && jsonData.props.pageProps.product) {
-
-    return jsonData.props.pageProps.product.popularDegree;
-    } else {
-      console.error('Invalid JSON data:', jsonData);
-      return null;
-    }  
-}
