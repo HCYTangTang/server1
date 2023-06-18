4e878e9abd73513df02eb091d903d7af38980f1d
HCYTangTang <134395256+HCYTangTang@users.noreply.github.com>
2023-06-18 18:10:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -38,9 +38,10 @@ app.get('/search/:keyword/:start', async (req, res) => {
 app.get('/search/:keyword/:storeName/:start', async (req, res) => {
   const { keyword, storeName, start } = req.params;
   try {
+    const apiUrl = "https://port-0-ykad-koh2xlikf0dph.sel4.cloudtype.app/search";
     const response = await axios.get(`${apiUrl}/${keyword}/${storeName}/${start}`);
     
-    res.json(response.data.items);
+    res.json(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error' });
