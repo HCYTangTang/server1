c9cbd4e7f0fe5549dc6e30702b5711d3fada449a
HCYTangTang <134395256+HCYTangTang@users.noreply.github.com>
2023-07-20 18:40:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const clientSecret = "r1Z3AkEecz";
 const apiUrl = "https://openapi.naver.com/v1/search/shop.json";
 
 // 키워드 검색 엔드포인트
-app.get('/search/:keyword/:productId/:start', async (req, res) => {
-  const { keyword, productId, start } = req.params;
+app.get('/search/:keyword/:start', async (req, res) => {
+  const { keyword, start } = req.params;
   try {
     const response = await axios.get(apiUrl, {
       headers: {
@@ -27,7 +27,6 @@ app.get('/search/:keyword/:productId/:start', async (req, res) => {
         display: 100,
       },
     });
-    const filteredItems = response.data.items.filter(item => item.productId === productId); 
     
     res.json(response.data.items);
   } catch (error) {
@@ -52,6 +51,30 @@ app.get('/rank/:keyword/:storeName/:start', async (req, res) => {
         display: 100,
       },
     });
+
+    res.json(response.data.items);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error' });
+  }
+});
+
+// 순위 추적 엔드포인트
+app.get('/:keyword/:productId/:start', async (req, res) => {
+  const { keyword, productId, start } = req.params;
+  try {
+    const response = await axios.get(apiUrl, {
+      headers: {
+        "X-Naver-Client-Id": clientId,
+        "X-Naver-Client-Secret": clientSecret,
+      },
+      params: {
+        query: keyword,
+        start: start,
+        display: 100,
+      },
+    });
+    const filteredItems = response.data.items.filter(item => item.productId === productId);
 
     res.json(response.data.items);
   } catch (error) {
